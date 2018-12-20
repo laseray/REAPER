e79efde4b94e7c3f78c892aaba1ca0e69ba974db
Raymond Martin <laseray@gmail.com>
2018-12-19 21:35:56 -0500

    Update project.clj
    Update and rearrange dependencies

--- project.clj
+++ project.clj
@@ -1,21 +1,21 @@
-(defproject reaper "0.1.0-SNAPSHOT"
+(defproject reaper "0.1.1-SNAPSHOT"
   :description "A project implementing automated summarization
                 as a submodular maximization problem."
   :url "https://github.com/codyrioux/reaper"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clojure-opennlp "0.2.2"]
-                 [hiccup "1.0.3"]
-                 [clj-wordnet "0.0.5"]
-                 [org.clojure/math.combinatorics "0.0.7"]
-                 [cc.mallet/mallet "2.0.7"]
-                 [incanter/incanter-core "1.5.0-SNAPSHOT"]
-                 [criterium "0.4.3"]
-                 [kmeans-clj "0.1.0-SNAPSHOT"]
-                 [com.taoensso/timbre "2.6.1"]
-                 [prismatic/plumbing "0.1.1"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [org.clojure/core.memoize "0.5.6"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.490"]
+                 [org.clojure/core.memoize "0.7.1"]
+                 [org.clojure/math.combinatorics "0.1.4"]                 
+                 [cc.mallet/mallet "2.0.8"]
+                 [clj-wordnet "0.1.0"]
+                 [clojure-opennlp "0.5.0"]                 
+                 [com.taoensso/timbre "4.10.0"]                 
+                 [criterium "0.4.4"]
+                 [hiccup "1.0.5"]
+                 [incanter/incanter-core "1.9.3"]                               
+                 [kmeans-clj "0.1.0-SNAPSHOT"]                                 
+                 [prismatic/plumbing "0.5.5"]]
   :main reaper.experiments.sample 
   :jvm-opts ["-Xmx2g" "-Xms2g" "-server"])
